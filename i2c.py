8b1cfb252e4810429ab9b6db539f4d23a369600b
Jasdeep  Singh <82466502+Jassingh6060@users.noreply.github.com>
2021-11-22 18:02:16 +0530

    Create i2c.py

---
+++ i2c.py
@@ -0,0 +1,29 @@
+import smbus
+import time
+
+device_address =  0x23
+
+one_time_high_res_mode = 0x20
+
+bus = smbus.SMBus(1)
+
+def read_light_value():
+    value = bus.read_i2c_block_data(device_address, one_time_high_res_mode)
+    return Convert(value)
+
+def Convert(data):
+    return ((data[1] + (256*data[0]))/1.2)
+
+while True:
+     Intensity = read_light_value()
+     time.sleep(1)
+     if(Intensity > 2500):
+         print("Too Bright")
+     elif(Intensity > 1500 and Intensity < 2500):
+         print("Bright")
+     elif(Intensity > 1000 and Intensity < 1500):
+         print("Medium")
+     elif(Intensity > 500 and Intensity < 1000):
+         print("Dark")
+     elif(Intensity < 500):
+         print("Too Dark")
